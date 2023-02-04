9cdbc9413d909dbd1fd2f754bceb9803898e7c64
Sergei Korolev <dromgard@yandex.ru>
2023-02-04 18:14:34 +0300

    feat: закрытие попапа по оверлею

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,14 +3,14 @@ import React from "react";
 function Popup({ isOpen, onClose, burgerMenu, children }) {
   return (
     <div
-      className={`popup ${isOpen ? "popup_opened" : ""} ${
-        burgerMenu ? "popup_burger-menu" : ""
-      }`}
+      className={`popup ${isOpen ? "popup_opened" : ""} ${burgerMenu ? "popup_burger-menu" : ""
+        }`}
+      onClick={onClose}
     >
       <div
-        className={`popup__container ${
-          burgerMenu ? "popup__container_burger-menu" : ""
-        }`}
+        className={`popup__container ${burgerMenu ? "popup__container_burger-menu" : ""
+          }`}
+        onClick={(e) => { e.stopPropagation() }}
       >
         {children}
         <button
